16b89a8ceeaed87c2bb7b9eab851c760098d2a9f
zzz7750260 <547926016@qq.com>
2018-01-04 18:48:26 +0800

    添加新模块

--- routes/admin.js
+++ routes/admin.js
@@ -2,6 +2,9 @@ var express = require('express');
 var router = express.Router();
 var mongoose = require('mongoose');
 var Admin = require('../models/admin');
+require('../util/dataUtil.js');
+var RandomId = require('../util/randomId.js');
+
 
 //连接MongoDB数据库
 mongoose.connect('mongodb://127.0.0.1:27017/adminDb');
@@ -23,5 +26,22 @@ router.get("/",function(req, res, next){
 	res.send('hellow,users . ');
 	
 });
+
+router.post("/registe", function(req, res, next){
+	let theUserID , 
+		theUserName = req.body.useName,
+		thePass = req.body.userPass,
+		theUserJoinTime ,
+		theUserQQ = req.body.userQQ,
+		theUserTel = req.body.userTel,
+		theUserEmail = req.body.userEmail,
+		theUserRole = req.body.userRole;
+	
+	//生成用户id
+		console.log(RandomId.theId());
+		
+})
+
+
 
 module.exports = router;
---
+++ util/randomId.js
@@ -0,0 +1,10 @@
+require('./dataUtil.js')
+function theId(){
+	var r1 = Math.floor(Math.random()*10);
+	var r2 = Math.floor(Math.random()*10);
+	var tid = "233";
+	var sysDate = new Date().Format('yyyyMMddhhmmss');
+	return theId = tid + r1 + sysDate + r2;
+}
+
+module.exports = theId;
